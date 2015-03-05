8a577df157e4b1cd96136ec7371eea88d0a55a78
RawTech <rawtechx@gmail.com>
2015-03-05 20:03:28 +0000

    Perlin noise.

--- src/RawTech/Perlin/Noise.php
+++ src/RawTech/Perlin/Noise.php
@@ -3,11 +3,164 @@
 namespace RawTech\Perlin;
 
 /**
- * Class Noise
+ * Generates Perlin noise.
  *
  * @package RawTech\Perlin
  */
 class Noise
 {
+    /**
+     * @var integer
+     */
+    private $r1;
 
+    /**
+     * @var integer
+     */
+    private $r2;
+
+    /**
+     * @var integer
+     */
+    private $r3;
+
+    /**
+     * @var float
+     */
+    private $persistence = 0.15;
+
+    /**
+     * @var integer
+     */
+    private $octaves = 1;
+
+    /**
+     * Construct.
+     *
+     * @param integer|null $r1
+     * @param integer|null $r2
+     * @param integer|null $r3
+     */
+    public function __construct($r1 = null, $r2 = null, $r3 = null)
+    {
+        if (!$r1) {
+            $this->r1 = rand(1000, 10000);
+        } else {
+            $this->r1 = (int) $r1;
+        }
+
+        if (!$r2) {
+            $this->r2 = rand(100000, 1000000);
+        } else {
+            $this->r2 = (int) $r2;
+        }
+
+        if (!$r3) {
+            $this->r3 = rand(1000000000, 2000000000);
+        } else {
+            $this->r3 = (int) $r3;
+        }
+    }
+
+    /**
+     * Gets noise for a 2D vector.
+     *
+     * @param float $x
+     * @param float $y
+     *
+     * @return float
+     */
+    public function noise2D($x, $y){
+        $total = 0;
+
+        for ($i = 0; $i < $this->octaves; $i++){
+            $frequency = pow(2, $i);
+            $amplitude = pow($this->persistence, $i);
+            $total = $total + $this->interpolatedNoise($x * $frequency, $y * $frequency) * $amplitude;
+        }
+
+        return -$total;
+    }
+
+    /**
+     * Interpolate noise for a 2D vector.
+     *
+     * @param float $x
+     * @param float $y
+     *
+     * @return float
+     */
+    public function interpolatedNoise($x, $y)
+    {
+        $integerX    = (int) $x;
+        $fractionalX = $x - $integerX;
+
+        $integerY    = (int) $y;
+        $fractionalY = $y - $integerY;
+
+        $v1 = $this->smoothedNoise($integerX,     $integerY);
+        $v2 = $this->smoothedNoise($integerX + 1, $integerY);
+        $v3 = $this->smoothedNoise($integerX,     $integerY + 1);
+        $v4 = $this->smoothedNoise($integerX + 1, $integerY + 1);
+
+        $i1 = $this->interpolate($v1 , $v2 , $fractionalX);
+        $i2 = $this->interpolate($v3 , $v4 , $fractionalX);
+
+        return $this->interpolate($i1 , $i2 , $fractionalY);
+    }
+
+    /**
+     * Smooth noise for a 2D vector.
+     *
+     * @param float $x
+     * @param float $y
+     *
+     * @return float
+     */
+    public function smoothedNoise($x, $y)
+    {
+        $corners = ($this->noise($x - 1, $y - 1) + $this->noise($x + 1, $y - 1) + $this->noise($x - 1, $y + 1) + $this->noise($x + 1, $y + 1)) / 16;
+        $sides   = ($this->noise($x - 1, $y) + $this->noise($x + 1, $y) + $this->noise($x, $y - 1) + $this->noise($x, $y + 1)) /  8;
+        $center  =  $this->noise($x, $y) / 4;
+
+        return $corners + $sides + $center;
+    }
+
+    /**
+     * Get noise for a 2D vector.
+     *
+     * @param float $x
+     * @param float $y
+     *
+     * @return float
+     */
+    public function noise($x, $y)
+    {
+        $x = intval($x);
+        $y = intval($y);
+        $n = $x + $y * 57;
+        $xl = ($n << 13) ^ $n;
+        $x2 = intval($xl * $xl * $this->r1 + $this->r2);
+        $t1 = intval($xl *  $x2);
+        $t2 = intval($t1 + $this->r3);
+        $t3 = $t2 & 0x7fffffff; // max 32 bit integer value.
+
+        return 1 - ($t3 / 1073741824.0);
+    }
+
+    /**
+     * Interpolates an alpha with a 2D vector.
+     *
+     * @param float $x
+     * @param float $y
+     * @param float $a
+     *
+     * @return float
+     */
+    public function interpolate($x, $y, $a)
+    {
+        $val = (1 - cos($a * pi())) * 0.5;
+
+        return $x * (1 - $val) + $y * $val;
+    }
 }
--- src/RawTech/Perlin/Tests/NoiseTest.php
+++ src/RawTech/Perlin/Tests/NoiseTest.php
@@ -21,11 +21,47 @@ class NoiseTest extends \PHPUnit_Framework_TestCase
      */
     public function setup()
     {
-        $this->noise = new Noise();
+        $this->noise = new Noise(1, 1, 1);
     }
 
-    public function testOne()
+    /**
+     * Tests noise generation.
+     *
+     * Assumes default seed.
+     */
+    public function testNoise()
     {
-        $this->assertSame(1,1);
+        $this->assertSame(
+            -0.966383549384772777557373046875,
+            $this->noise->noise(1, 1)
+        );
+        $this->assertSame(
+            0.125139863230288028717041015625,
+            $this->noise->noise(1, 2)
+        );
+        $this->assertSame(
+            0.528616565279662609100341796875,
+            $this->noise->noise(1, 3)
+        );
+    }
+
+    /**
+     * Test alpha interpolation.
+     *
+     * Assumes default seed.
+     */
+    public function testInterpolation()
+    {
+        $this->assertSame(1.0, $this->noise->interpolate(1, 1, 1.0));
+        $this->assertSame(1.0, $this->noise->interpolate(1, 1, 0.1));
+
+        $this->assertSame(
+            1.195491502812526363186407252214848995208740234375,
+            $this->noise->interpolate(1.1, 2.1, 0.2)
+        );
+        $this->assertSame(
+            1.000000024674010834502269062795676290988922119140625,
+            $this->noise->interpolate(1, 2, 0.0001)
+        );
     }
 }
